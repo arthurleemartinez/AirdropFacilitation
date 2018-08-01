802c77653587005c1c9bf543464482f1c9611bb1
arthurleemartinez <40931653+arthurleemartinez@users.noreply.github.com>
2018-08-01 18:09:34 -0500

    Update column_to_list.py

--- column_to_list.py
+++ column_to_list.py
@@ -5,13 +5,14 @@ list_column = []
 def get_user_split_column(user_init_column):
 	# every ethereum address is equal to 42 char
 	n = 42
-	for i in range(0, len(line), n):
-		usc = [user_init_column[i:i+n]]
+	# iterate in list and seperate it by ethereum address
+	from every addressfor i in range(0, len(line), n):
+	    usc = [user_init_column[i:i+n]]
 	return usc
 user_split_column = get_user_split_column(user_init_column
 def remove_0x(user_split_column):
-		address_removed_prefix = user_split_column[2:-2]		  
-    	return address_removed_prefix										  
+	address_removed_prefix = user_split_column[2:-2]		  
+    return address_removed_prefix										  
 rem_0x = remove_0x(user_split_column)
 
 del column_csv(rem_0x):

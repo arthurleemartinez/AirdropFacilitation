6f70fbf4f83cc7163a619ab9140fd281f0f604d6
arthurleemartinez <40931653+arthurleemartinez@users.noreply.github.com>
2018-08-01 17:58:19 -0500

    Update column_to_list.py

--- column_to_list.py
+++ column_to_list.py
@@ -1,5 +1,18 @@
-column = ""
+# initialize user interface
+print("This program will help you perform your airdrop.")
+user_column = num(input("Please enter/paste your entire column of ethereum addresses.")
 list_column = []
-del column_csv():
-	for item in 
-	
+def get_user_split_column(user_init_column):
+	# every ethereum address is equal to 42 char
+	n = 42
+	for i in range(0, len(line), n):
+		usc = [user_init_column[i:i+n]]
+	return usc
+user_split_column = get_user_split_column(user_init_column
+def remove_0x(user_split_column):
+		address_removed_prefix = user_split_column[2:-2]		  
+    	return address_removed_prefix										  
+rem_0x = remove_0x(user_split_column)
+
+del column_csv(rem_0x):
+	for address in rem_0x:

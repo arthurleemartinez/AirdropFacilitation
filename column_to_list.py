7a3ae163b64dc7bc944624f29a372c3bf169572d
arthurleemartinez <40931653+arthurleemartinez@users.noreply.github.com>
2018-08-01 23:52:41 -0500

    Update column_to_list.py

--- column_to_list.py
+++ column_to_list.py
@@ -3,17 +3,21 @@ from typing import List, Any
 
 print("This program will help you perform your airdrop.")
 user_column: str = input("Please enter/paste your entire column of ethereum addresses.")
+user_column = user_column[:75]
 list_column = []
 # every ethereum address is equal to 42 char
 n = 42
 # iterate in list and separate it by ethereum address
 def get_user_split_column():
+    a = []
+    """
+    :rtype: list
+    """
     for i in range(0, len(user_column), n):
+        assert isinstance(Any, list)
         usc: List[ Any] = (user_column[i:i + n], n)
         a: List[Any] = usc
     return a
+
 user_split_column = get_user_split_column()
-def remove_0x():
-    address_removed_prefix = user_split_column[2:-2]		  
-    return address_removed_prefix
-rem_0x = remove_0x()
+print(get_user_split_column())

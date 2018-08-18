90f81a6730db59fa8511f418444ca4a03ed04779
Arthur Martinez <40931653+arthurleemartinez@users.noreply.github.com>
2018-08-17 20:18:06 -0500

    Update arbitrary_values.py

--- arbitrary_values.py
+++ arbitrary_values.py
@@ -1,9 +1,8 @@
 import random
-random1 = random.random()
 x = []
 y = []
 for i in range(100):
-    y = random1 * 5000
+    y = random.random() * 7500
     zz = round(y, 6)
     x.append(zz)
 print(x)
